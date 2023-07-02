81d79e5b2ef5372317c8c9840ab4734f4e7e39aa
Nikolas <lihonenko33.niko@gmail.com>
2023-07-02 09:44:17 +0500

    Homework 7

---
+++ homework 7/ex2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+System.Console.WriteLine(" введите номер  строки...");
+int line = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(" ввести номер  столбца...");
+int column = Convert.ToInt32(Console.ReadLine());
+int  [ , ] array = new int [10,10];       
+FillArrayRandomArray(array);                                 
+PrintArray(array);     
+if(line>array.GetLength(0)|| column>array.GetLength(1))
+{ 
+    System.Console.WriteLine(" такого элемента НЕТ...");
+}
+else {
+    System.Console.WriteLine($" значение элемента строки и столбца { line} , {column} равно {array[line-1, column-1]}");
+}
+void FillArrayRandomArray(int [ , ] array)
+{
+    for(int i =0; i<array.GetLength(0); i++)
+    {
+        for(int j =0; j<array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(-100,100)/10;
+        }
+    }
+}
+void PrintArray(int[ , ] array)
+{
+    for(int i=0; i<array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1); j++)
+        {
+            System.Console.Write(array [i,j] + ". ");
+        }
+        System.Console.WriteLine();
+    }
+}
---
+++ homework 7/ex3/Program.cs
@@ -0,0 +1,47 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+System.Console.WriteLine(" введите количество строк...");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(" ввести количество столбцов...");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine();
+int [ , ] array = new int [linesVol, columnsVol];
+FillArrayRandomArray(array);
+PrintArray(array); 
+for(int j =0; j<array.GetLength(1); j++) 
+{
+    int mean = 0;
+    for(int i =0; i<array.GetLength(0); i++) 
+    {
+        mean = (mean + array[i,j]);
+    }
+    mean = mean/columnsVol;
+    System.Console.Write(mean + ";");
+}
+System.Console.WriteLine();
+void FillArrayRandomArray(int[ , ] array)
+{
+    for(int i =0; i<array.GetLength(0); i++)
+    {
+        for(int j =0; j<array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(0,10);
+        }
+    }
+}
+void PrintArray(int[ , ] array)
+{
+    for(int i=0; i<array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1); j++)
+        {
+            System.Console.Write(array [i,j]+", ");
+        }
+        System.Console.WriteLine();
+    }
+}
