f0fe90ded429ff00d576f9396e365ff9ba163c3e
Nikolas <lihonenko33.niko@gmail.com>
2023-07-09 22:02:52 +0500

    Homework 9

---
+++ Homework 9/ex2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNum(int m, int n)
+{
+    if(m < n)
+    {
+        return SumNum(m + 1, n) + m;
+    }
+    else if (m > n)
+    {
+        return SumNum(m - 1, n) + m;
+    } 
+    else return m;
+}
+
+Console.WriteLine("Введите значение M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(SumNum(m, n));
---
+++ Homework 9/ex3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана 
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int FuncAccerman(int m, int n)
+{
+    if(m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return FuncAccerman(m - 1, 1);
+    }
+    else
+    {
+        return FuncAccerman(m - 1,FuncAccerman(m, n - 1));
+    }
+} 
+
+Console.WriteLine("Введите значение M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(FuncAccerman(m, n));
